f15aed40ced9c3d875d539a7a955524304b82a59
2003bromy <107309074+2003bromy@users.noreply.github.com>
2022-10-19 11:32:53 +0300

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -18,7 +18,7 @@ job('NodeJS example') {
 }
 job('Alex example') {
     scm {
-        git('git://github.com/2003romy/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/2003bromy/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL alex')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
