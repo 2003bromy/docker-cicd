838dae30d1bf17446b660f4fa395ac40f8185b71
2003bromy <107309074+2003bromy@users.noreply.github.com>
2022-10-19 11:24:33 +0300

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -16,4 +16,23 @@ job('NodeJS example') {
         shell("npm install")
     }
 }
+job('Alex example') {
+    scm {
+        git('git://github.com/2003romy/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL yaniv')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
+
 
